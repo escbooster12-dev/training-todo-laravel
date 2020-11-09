a2a6cdb2724275d7c02a35d75a0b2b4bb7f10c02
Joshua <joshua.escarilla@sun-asterisk.com>
2020-11-09 10:59:26 +0800

    create 'todo' migration

--- database/migrations/2020_11_09_024118_create_todos_table.php
+++ database/migrations/2020_11_09_024118_create_todos_table.php
@@ -15,6 +15,8 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->char('task');
+            $table->timestamp('datetime');
             $table->timestamps();
         });
     }
